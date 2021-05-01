51bdcd3631899a5dec58b2080b7f6b469872f92b
SeugnshinLee <lsver67@gmail.com>
2021-05-01 15:38:44 +0900

    이름 변경 및 figure 파일

--- bluetooth_Send.m
+++ bluetooth_Send.m
@@ -13,22 +13,29 @@ while 1
     write(device,command,"int8");
     
     % 알티노로 부터 응답 읽기
-    reply = read(device,1,"string")
+    for ii = 1:10
+        
+        reply = read(device,1,"string");
+        switch reply
+            case '0' 
+                fprintf("reply = %s\n", reply);
+                break;
+            
+            case 'e'
+                fprintf("reply = %s\n", reply);
+                break;
+            
+            case 's'
+                fprintf("reply = %s\n", reply);
+                break;
+            
+        end
+    end
+    
     
     % 알티노는 1바이트 응답을 하므로 그 뒤는 모두 가비지 값
     % 삭제
-    flush(device,"input");
-    
-    % 알티노로 부터 오류를 보고 받았을때
-    if reply ~= '0'
-        for ii = 1:4
-        reply = readline(device);
-        disp(reply);
-    
-        ii = ii + 1;
-        end
-    end
+    flush(device);
  
-    
 end
 
