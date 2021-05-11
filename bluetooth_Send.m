9cb5571e4f21e391dcc8f77380b3c4833ea14d9b
SeugnshinLee <lsver67@gmail.com>
2021-05-11 10:25:07 +0900

    센서값 수신 코드 완성

--- bluetooth_Send.m
+++ bluetooth_Send.m
@@ -18,6 +18,10 @@ Carshape = polyshape([-50 -50 50 50],[ 90 -90 -90 90]);
 
 
 while 1
+    job = batch('SensorRead')
+    disp(recieve)
+    
+    
     % 속도명령 입력받음
     speed = input("speed: ");
     
@@ -38,46 +42,12 @@ while 1
     Steer_L = steer - bitshift(Steer_H, 7);
     
     % 입력받은 명령들을 패킷화
-    command = [2 Speed_H Speed_L Steer_H Steer_L 65 65 65 65 3];
+    command = [2 Speed_H Speed_L Steer_H Steer_L 65 65 65 3];
     
     % 블루투스 시리얼로 int8형식으로 전송
     write(device,command,"int8");
     
-    % 알티노로 부터 응답 읽기
-    for ii = 1:10
-        
-        reply = read(device,1,"string");
-        switch reply
-            case '0' 
-                fprintf("reply = %s\n", reply);
-                
-                % 센서값 추출 부분
-                sensor = read(device,10,"uint8");
-                front = bitshift(sensor(2), 8) + sensor(3);
-                
-                % 그래프 그리기
-                plot(front,'*')
-                drawnow;
-                axis square
-                axis([-500 500 -500 500]);
-                
-                break;
-            
-            case 'e'
-                fprintf("reply = %s\n", reply);
-                break;
-            
-            case 's'
-                fprintf("reply = %s\n", reply);
-                break;
-            
-        end
-    end
     
-    
-    % 알티노는 1바이트 응답을 하므로 그 뒤는 모두 가비지 값
-    % 삭제
-    flush(device);
  
 end
 
