8cd726feeffea9e6a5073c03fed3f6b107cd514b
SeugnshinLee <lsver67@gmail.com>
2021-05-15 16:43:23 +0900

    주석 추가 및 정지 버튼 추가

--- KeyPressCallback.m
+++ KeyPressCallback.m
@@ -6,19 +6,29 @@ global myVariableX;
 mode = event.Character;
 
 switch mode
+    % ESC
     case char(27)
        myVariable = 0;
        
+    % 전진 방향키   
     case char(30)
         myVariable = myVariable + 0.1;
         
+    % 후진  
     case char(31)
         myVariable = myVariable - 0.1;
-        
+    
+    % 좌측
     case(28)
         myVariableX = myVariableX - 0.1;
         
+    % 우측    
     case(29)
         myVariableX = myVariableX + 0.1;
+    
+    % 스페이스바
+    case(32)
+        myVariable = 0;
+        myVariableX = 0;
         
 end
